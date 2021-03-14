44d43d8ba00160b90afa860ca32e8435bcbf4941
SvenKim <svenkim_zhuo@qq.com>
2021-03-15 01:58:04 +0800

    push audio OK

--- out/ffmpeg_enc_audio.cpp
+++ out/ffmpeg_enc_audio.cpp
@@ -5,32 +5,41 @@ extern "C"
 #include <libavcodec/avcodec.h>
 #include <libavformat/avformat.h>
 #include <libswresample/swresample.h>
+#include <libavutil/opt.h>
+#include <libavutil/time.h>
 }
 
 static void get_adts_header(AVCodecContext *ctx, uint8_t *adts_header, int aac_length);
 
 int main(int argc, char *argv[])
 {
-    if (argc <= 1)
+    if (argc <= 2)
     {
-        std::cout << argv[0] << " <input PCMfile> " << std::endl;
+        std::cout << argv[0] << " <input PCMfile> <output url>" << std::endl;
         exit(-1);
     }
 
     /* Parameters Definition */
-    const char *input;
+    const char *input, *output;
     input = argv[1];
+    output = argv[2];
 
     AVCodec *mAudioCodec;
     AVCodecContext *mAudioCodecCtx;
     AVPacket *mAudioPacket;
     AVFrame *mAudioFrame;
+    SwrContext *mAudioSwrCtx;
+
+    /* Output Stream */
+    AVFormatContext *mFormatCtx;
+    AVStream *OutAudioStream;
 
     uint8_t *mAudioFrameBuf; // Get From User
     int mAudioInFrameLength = 0;
     int mAudioOutFrameLength = 0; // Length of A Frame
     uint8_t **mAudioConvertData;  // Convert to AVFrame Data
 
+    int mAudioStreamIndex = -1;
     int ret;
 
     av_register_all();
@@ -65,11 +74,11 @@ int main(int argc, char *argv[])
 
     mAudioPacket = av_packet_alloc();
     if (!mAudioPacket)
-        throw std::runtime_error("Error: Could not allocate audio/video packet");
+        throw std::runtime_error("Error: Could not allocate audio packet");
 
     mAudioFrame = av_frame_alloc();
     if (!mAudioFrame)
-        throw std::runtime_error("Error: Could not allocate audio/video frame");
+        throw std::runtime_error("Error: Could not allocate audio frame");
 
     mAudioFrame->format = mAudioCodecCtx->sample_fmt;
     mAudioFrame->nb_samples = mAudioCodecCtx->frame_size;
@@ -78,12 +87,12 @@ int main(int argc, char *argv[])
 #if 1
     ret = av_frame_get_buffer(mAudioFrame, 32);
     if (ret < 0)
-        throw std::runtime_error("Error: Could not allocate the audio/video frame data");
+        throw std::runtime_error("Error: Could not allocate the audio frame data");
 #endif
 
     // PCM ReSample: Input PCM must be 44100kHz + signed_16_bits + double_channels
     // Change: S16 -> FLTP
-    SwrContext *mAudioSwrCtx = swr_alloc();
+    mAudioSwrCtx = swr_alloc();
 
     swr_alloc_set_opts(mAudioSwrCtx,
                        av_get_default_channel_layout(mAudioCodecCtx->channels),
@@ -106,6 +115,63 @@ int main(int argc, char *argv[])
 
     /* ==================================== */
 
+    /*================== Pusher Init ==================*/
+    ret = avformat_alloc_output_context2(&mFormatCtx, NULL, "RTSP", output); //RTSP
+    if (ret < 0 || !mFormatCtx)
+        throw std::runtime_error("Error: fail to create output context");
+
+    av_opt_set(mFormatCtx->priv_data, "rtsp_transport", "tcp", 0); //Transport With TCP
+
+#if 0
+    //检查所有流是否都有数据，如果没有数据会等待max_interleave_delta微秒
+    mFormatCtx->max_interleave_delta = 1000000;
+#endif
+
+    /* Create Audio output AVStream */
+    OutAudioStream = avformat_new_stream(mFormatCtx, mAudioCodec);
+    if (!OutAudioStream)
+        throw std::runtime_error("Error: fail to allocating output stream");
+
+#if 1
+    OutAudioStream->time_base = {1, 25};
+    mAudioStreamIndex = OutAudioStream->id = mFormatCtx->nb_streams - 1; //加入到fmt_ctx流
+#endif
+
+    /* Copy the settings of AVCodecContext */
+    ret = avcodec_copy_context(OutAudioStream->codec, mAudioCodecCtx);
+    if (ret < 0)
+        throw std::runtime_error("Error: fail to avcodec_copy_context");
+
+    OutAudioStream->codec->codec_tag = 0;
+    if (mFormatCtx->oformat->flags & AVFMT_GLOBALHEADER)
+        OutAudioStream->codec->flags |= AV_CODEC_FLAG_GLOBAL_HEADER;
+
+#if 0
+    avcodec_parameters_from_context(OutAudioStream->codecpar, mAudioCodecCtx);
+#endif
+
+    av_dump_format(mFormatCtx, 0, mFormatCtx->filename, 1); // Dump Format
+
+    /* Open output URL */
+    if (!(mFormatCtx->oformat->flags & AVFMT_NOFILE))
+    {
+        ret = avio_open(&mFormatCtx->pb, mFormatCtx->filename, AVIO_FLAG_WRITE);
+        if (ret < 0)
+        {
+            fprintf(stderr, "Could not open output URL '%s'", mFormatCtx->filename);
+            throw std::runtime_error("Error: fail to avio_open");
+        }
+    }
+
+#if 1 // Important~ From avformat_new_stream() * When muxing, should be called by the user before avformat_write_header().
+    /* Write file header */
+    ret = avformat_write_header(mFormatCtx, NULL);
+    if (ret < 0)
+        throw std::runtime_error("Error occurred when opening output URL");
+
+    long start_time = av_gettime();
+#endif
+
     /*================== Encode Frame To Packet ==================*/
 
 #if 1
@@ -117,10 +183,12 @@ int main(int argc, char *argv[])
         throw std::runtime_error("Error: fail to open file");
 
     int counts = 0;
+    int frame_index = 0;
 #endif
 
     while (1)
     {
+        av_usleep(1000 * 20);
         // 输入一帧数据的长度
         mAudioInFrameLength = mAudioFrame->nb_samples * av_get_bytes_per_sample(AV_SAMPLE_FMT_S16) * mAudioFrame->channels;
 
@@ -144,7 +212,7 @@ int main(int argc, char *argv[])
         memcpy(mAudioFrame->data[0], mAudioConvertData[0], mAudioOutFrameLength);
         memcpy(mAudioFrame->data[1], mAudioConvertData[1], mAudioOutFrameLength);
 
-        mAudioFrame->pts = 100 * counts++;
+        mAudioFrame->pts = counts++ /* *100 */;
 
         /* send the frame to the encoder */
         if (mAudioFrame)
@@ -172,9 +240,83 @@ int main(int argc, char *argv[])
             fwrite(aac_header, 1, 7, fp_out);
             fwrite(mAudioPacket->data, 1, mAudioPacket->size, fp_out);
 
+#if 1 //push~~
+            mAudioPacket->stream_index = mAudioStreamIndex;
+            if (mAudioPacket->pts == AV_NOPTS_VALUE)
+            {
+                //Write PTS
+                AVRational time_base1 = mAudioCodecCtx->time_base; // ifmt_ctx->streams[audioindex]->time_base;
+                //Duration between 2 frames (us)
+                int64_t calc_duration = (double)AV_TIME_BASE / av_q2d(mAudioCodecCtx->framerate /*ifmt_ctx->streams[audioindex]->r_frame_rate*/);
+                //Parameters
+                mAudioPacket->pts = (double)(frame_index * calc_duration) / (double)(av_q2d(time_base1) * AV_TIME_BASE);
+                mAudioPacket->dts = mAudioPacket->pts;
+                mAudioPacket->duration = (double)calc_duration / (double)(av_q2d(time_base1) * AV_TIME_BASE);
+            }
+
+            //Important:Delay
+            if (mAudioPacket->stream_index == mAudioStreamIndex)
+            {
+                AVRational time_base = mAudioCodecCtx->time_base; // ifmt_ctx->streams[audioindex]->time_base;
+                AVRational time_base_q = {1, AV_TIME_BASE};
+                int64_t pts_time = av_rescale_q(mAudioPacket->dts, time_base, time_base_q);
+                int64_t now_time = av_gettime() - start_time;
+                if (pts_time > now_time)
+                    av_usleep(pts_time - now_time);
+            }
+
+            /* copy packet */
+            //Convert PTS/DTS
+            mAudioPacket->pts = av_rescale_q_rnd(mAudioPacket->pts, mAudioCodecCtx->time_base, OutAudioStream->time_base, (AVRounding)(AV_ROUND_NEAR_INF | AV_ROUND_PASS_MINMAX));
+            mAudioPacket->dts = av_rescale_q_rnd(mAudioPacket->dts, mAudioCodecCtx->time_base, OutAudioStream->time_base, (AVRounding)(AV_ROUND_NEAR_INF | AV_ROUND_PASS_MINMAX));
+            mAudioPacket->duration = av_rescale_q(mAudioPacket->duration, mAudioCodecCtx->time_base, OutAudioStream->time_base);
+            mAudioPacket->pos = -1;
+            //Print to Screen
+            if (mAudioPacket->stream_index == mAudioStreamIndex)
+            {
+                printf("Send %8d audio frames to output URL\n", frame_index);
+                frame_index++;
+            }
+
+            ret = av_interleaved_write_frame(mFormatCtx, mAudioPacket);
+
+            if (ret < 0)
+            {
+                printf("Error muxing packet\n");
+                break;
+            }
+#endif
+
             av_packet_unref(mAudioPacket);
         }
     }
+
+    /* Release resources */
+    if (mAudioFrameBuf)
+        free(mAudioFrameBuf);
+    if (mAudioConvertData)
+        av_freep(&mAudioConvertData[0]);
+    if (mAudioFrame)
+        av_frame_free(&mAudioFrame);
+    if (mAudioPacket)
+        av_packet_free(&mAudioPacket);
+    if (mAudioSwrCtx)
+        swr_free(&mAudioSwrCtx);
+    if (mAudioCodecCtx)
+        avcodec_free_context(&mAudioCodecCtx);
+    if (mFormatCtx)
+    {
+        if (!(mFormatCtx->oformat->flags & AVFMT_NOFILE))
+            avio_close(mFormatCtx->pb);
+        avformat_free_context(mFormatCtx);
+    }
+
+    if (fp_PCM)
+        fclose(fp_PCM);
+    if (fp_out)
+        fclose(fp_out);
+
+    return 0;
 
     return 0;
 }
